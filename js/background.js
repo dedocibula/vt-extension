26642d4f9790204f135897b868d0a49d6d5cd16f
DedoCibula <dedo.cibula@gmail.com>
2015-11-27 13:27:24 -0500

    added object stubs

--- js/background.js
+++ js/background.js
@@ -1,10 +1,49 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var url = chrome.extension.getURL('index.html');
-	chrome.tabs.query({ url: url }, function(tabs) {
-        if (tabs.length !== 0) {
-            chrome.tabs.update(tabs[0].id, { url: url, active: true });
-        } else {
-            chrome.tabs.create({ url: url });
-        }
-    });
-});
+(function($, window, document, undefined) {
+	var BackgroundWorker = (function() {
+		function BackgroundWorker(loader, storage) {
+			this.loader = loader;
+			this.storage = storage;
+		}
+
+		BackgroundWorker.prototype = {
+			// methods...
+		};
+
+		return BackgroundWorker;
+	})();
+
+	var Loader = (function() {
+		function Loader(options) {
+			this.options = $.extend({}, options);
+		}
+
+		Loader.prototype = {
+			// methods...
+		};
+
+		return Loader;
+	})();
+
+	var Storage = (function() {
+		function Storage() {
+		}
+
+		Storage.prototype = {
+			// methods...
+		};
+
+		return Storage;
+	})();
+
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		var url = chrome.extension.getURL('index.html');
+		chrome.tabs.query({ url: url }, function(tabs) {
+			if (tabs.length !== 0) {
+				chrome.tabs.update(tabs[0].id, { url: url, active: true });
+			} else {
+				chrome.tabs.create({ url: url });
+			}
+		});
+	});
+})(jQuery, window, document);
+
---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function($, window, document, undefined) {
+	var Controller = (function() {
+		function Controller(renderer) {
+			this.renderer = renderer;
+		}
+
+		Controller.prototype = {
+			// methods...
+		};
+
+		return Controller;
+	})();
+
+	var Renderer = (function() {
+		function Renderer() {
+		}
+
+		Renderer.prototype = {
+
+		};
+
+		return Renderer;
+	})();
+})(jQuery, window, document);
