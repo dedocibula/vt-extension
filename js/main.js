d6880204e0bb0d7eef2c40f7163997d8bf3cf49a
DedoCibula <dedo.cibula@gmail.com>
2015-11-30 21:50:12 -0500

    added Handlebars dependency to main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function($, window, document, undefined) {
+(function($, Handlebars, window, document, undefined) {
 	var Controller = (function() {
 		function Controller(backend, renderer) {
 			this.backend = backend;
@@ -26,7 +26,9 @@
 	// initialize objects
 	var backend = chrome.extension.getBackgroundPage();
 	var controller = new Controller(new Renderer());
+
+	// temporary
 	backend.getLatestResults(function(results) {
 		console.log(results);
 	});
-})(jQuery, window, document);
+})(jQuery, Handlebars, window, document);
