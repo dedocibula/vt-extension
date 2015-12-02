b5c1d43672447b8ef2f82ee8482581a0b828c089
DedoCibula <dedo.cibula@gmail.com>
2015-12-01 23:24:06 -0500

    added initial renderer code

--- js/main.js
+++ js/main.js
@@ -6,14 +6,32 @@
 		}
 
 		Controller.prototype = {
-			// methods...
+			invalidateAll: function() {
+				var self = this;
+				self.backend.getLatestResults(function(results) {
+					// todo if (results.loggedIn)
+
+				});
+			}
 		};
 
 		return Controller;
 	})();
 
 	var Renderer = (function() {
-		function Renderer() {
+		function Renderer(elements) {
+			// menu
+			this.$campusContainer = $(elements.campusContainer);
+			this.$termContainer = $(elements.termContainer);
+			this.$subjectContainer = $(elements.subjectContainer);
+
+			this.menuTemplate = Handlebars.compile(elements.menuTemplate);
+
+			// courses
+			this.$allSection = $(elements.allSection);
+			this.$watchedSection = $(elements.watchedSection);
+
+			this.courseTemplate = Handlebars.compile(elements.courseTemplate);
 		}
 
 		Renderer.prototype = {
@@ -25,7 +43,17 @@
 
 	// initialize objects
 	var backend = chrome.extension.getBackgroundPage();
-	var controller = new Controller(backend, new Renderer());
+	var renderer = new Renderer({
+		campusContainer: '#campus-container',
+		termContainer: '#term-container',
+		subjectContainer: '#subj-container',
+		allSection: '#all-courses .tbody',
+		watchedSection: '#observed-courses .tbody',
+
+		menuTemplate: $('#menuTemplate').html(),
+		courseTemplate: $('#courseTemplate').html()
+	});
+	var controller = new Controller(backend, renderer);
 
 	// temporary
 	backend.getLatestResults(function(results) {
