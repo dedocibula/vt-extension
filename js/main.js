698f475f06fb68325cdf6fc6d2927a6a719721ba
DedoCibula <dedo.cibula@gmail.com>
2015-12-02 00:48:06 -0500

    added initial menu rendering logic and initial controller logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 (function($, Handlebars, window, document, undefined) {
+	var elements = {
+		campusContainer: '#campus-container',
+		termContainer: '#term-container',
+		subjectContainer: '#subj-container',
+		allSection: '#all-courses .tbody',
+		watchedSection: '#observed-courses .tbody',
+
+		menuTemplate: $('#menuTemplate').html(),
+		courseTemplate: $('#courseTemplate').html()
+	};
+
 	var Controller = (function() {
 		function Controller(backend, renderer) {
 			this.backend = backend;
@@ -9,9 +20,23 @@
 			invalidateAll: function() {
 				var self = this;
 				self.backend.getLatestResults(function(results) {
-					// todo if (results.loggedIn)
-
+					if (!self._validResults(results)) return;
+					self menu = self.renderer.renderMenu(results.menu);
+					self._setupPreferences(results.preferences, results.default);
 				});
+			},
+
+			_validResults: function(results) {
+				// temporary
+				return $.isPlainObject(results) && results.loggedIn && $.isPlainObject(results.menu) &&
+					results.default && $.isPlainObject(results.preferences);
+			},
+
+			_setupPreferences: function(preferences, newTerm) {
+				var self = this;
+				
+				self.menu.$termMenu.data('preferences', preferences);
+				if (newTerm) self.menu.$termMenu.val(newTerm);
 			}
 		};
 
@@ -35,7 +60,23 @@
 		}
 
 		Renderer.prototype = {
+			renderMenu: function(menuResults) {
+				var self = this, menu = {};
 
+				menu.$campusMenu = $(menuTemplate({ CAMPUS: menuResults.CAMPUS })).appendTo(self.$campusContainer.empty());
+				menu.$subjectMenu = $(menuTemplate({ subj_code: menuResults.subj_code })).appendTo(self.$subjectContainer.empty());
+				menu.$termMenu = $(menuTemplate({ TERMYEAR: menuResults.TERMYEAR })).appendTo(self.$termContainer.empty());
+				menu.$termMenu.on('change', function() {
+					var preferences = menu.$termMenu.data('preferences') || {};
+					var currentTerm = $(this).val();
+					if (preferences[currentTerm]) {
+						menu.$campusMenu.val(preferences[currentTerm].CAMPUS);
+						menu.$subjectMenu.val(preferences[currentTerm].subj_code);
+					}
+				}));
+
+				return menu;
+			}
 		};
 
 		return Renderer;
@@ -43,16 +84,7 @@
 
 	// initialize objects
 	var backend = chrome.extension.getBackgroundPage();
-	var renderer = new Renderer({
-		campusContainer: '#campus-container',
-		termContainer: '#term-container',
-		subjectContainer: '#subj-container',
-		allSection: '#all-courses .tbody',
-		watchedSection: '#observed-courses .tbody',
-
-		menuTemplate: $('#menuTemplate').html(),
-		courseTemplate: $('#courseTemplate').html()
-	});
+	var renderer = new Renderer(elements);
 	var controller = new Controller(backend, renderer);
 
 	// temporary
